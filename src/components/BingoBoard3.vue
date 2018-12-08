8f7accfdff1fe7f9d9ba856c9a936479141f0bbe
ecrow <eli@reusserdesign.com>
2018-12-08 10:53:00 -0500

    Setup project structure

---
+++ src/components/BingoBoard3.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="BingoBoard3">
+		this is the bingo board
+	</div>
+</template>
+
+
+
+<script>
+export default {
+	name: 'BingoBoard3',
+	props: {
+
+	}
+}
+</script>
+
+
+
+<style scoped>
+.BingoBoard3 {
+
+}
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="MainPage">
+		<BingoBoard3/>
+	</div>
+</template>
+
+
+
+<script>
+import BingoBoard3 from '@/components/BingoBoard3'
+export default {
+	name: 'MainPage',
+	components: {
+		BingoBoard3
+	},
+	props: {
+
+	}
+}
+</script>
+
+
+
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
+.MainPage {
+
+}
+</style>
