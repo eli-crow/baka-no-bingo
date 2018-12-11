e5ac6abee40b0804d36850eb51b3cd533d332a9f
ecrow <eli@reusserdesign.com>
2018-12-10 21:23:34 -0500

    Update global styles

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,24 @@ export default {
 
 <style>
   :root {
+    --color-theme-black: hsl(0, 0%, 5%);
     --color-theme-gray: hsl(0, 0%, 67%);
     --color-theme-gray-light: hsl(0, 0%, 97%);
     --color-theme-blue: hsl(204, 100%, 50%);
     --color-theme-blue-light: hsl(204, 100%, 94%);
     --color-theme-red: hsl(0, 100%, 64%);
     --color-theme-green: hsl(146, 97%, 35%);
+    --color-theme-green-light: hsl(146, 90%, 44%);
     --color-theme-yellow: hsl(54, 100%, 50%);
     --color-theme-yellow-light: hsl(54, 100%, 90%);
 
-    --color-text-dark: hsl(0, 0%, 5%);
+    --color-text-dark: var(--color-theme-black);
     --color-text-default: hsl(0, 0%, 13%);
 
     --font-family-hiragino: "Hiragino Sans", "Hiragino Kaku Gothic Pro", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
-    --font-title: 800 1.25rem / 1.3 var(--font-family-hiragino);
-    --font-default: 400 1rem / 1.5 var(--font-family-hiragino);
+    --font-family-noto: "Noto Sans", "Hiragino Kaku Gothic Pro", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
+    --font-title: 800 1.25rem / 1.3 var(--font-family-noto);
+    --font-default: 400 1rem / 1.5 var(--font-family-noto);
 
     --line-groove: solid 2px hsl(0, 0%, 98%);
   }
