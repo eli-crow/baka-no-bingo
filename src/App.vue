ba6ab1de9f42597d400294f15cb6f0ad3325691f
ecrow <eli@reusserdesign.com>
2018-12-13 16:19:30 -0500

    Update variables

--- src/App.vue
+++ src/App.vue
@@ -14,20 +14,25 @@ export default {
 
 <style>
   :root {
+    --color-theme-white: hsl(0, 0%, 100%);
     --color-theme-black: hsl(0, 0%, 5%);
     --color-theme-gray: hsl(0, 0%, 67%);
-    --color-theme-gray-light: hsl(0, 0%, 95%);
+    --color-theme-gray-light: hsl(0, 0%, 83%);
+    --color-theme-gray-lightest: hsl(0, 0%, 95%);
     --color-theme-blue: hsl(204, 100%, 50%);
+    --color-theme-blue-dark: hsl(212, 100%, 50%);
     --color-theme-blue-light: hsl(204, 100%, 78%);
     --color-theme-red: hsl(0, 100%, 64%);
     --color-theme-red-dark: hsl(0, 71%, 51%);
     --color-theme-green: hsl(146, 97%, 35%);
     --color-theme-green-light: hsl(146, 90%, 44%);
-    --color-theme-yellow: hsl(54, 100%, 50%);
+    --color-theme-yellow: hsl(52, 100%, 50%);
+    --color-theme-yellow-dark: hsl(46, 100%, 50%);
     --color-theme-yellow-light: hsl(54, 100%, 85%);
 
     --color-text-dark: var(--color-theme-black);
     --color-text-default: hsl(0, 0%, 13%);
+    --color-text-light: hsl(0, 0%, 42%);
 
     --font-family-hiragino: "Hiragino Sans", "Hiragino Kaku Gothic Pro", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
     --font-family-noto: "Noto Sans", "Hiragino Kaku Gothic Pro", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
