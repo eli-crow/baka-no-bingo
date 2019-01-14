e895454a54160b07b963d5fcd098618728dee4e8
Eli Crow <elicrow@Elis-MacBook-Pro.local>
2019-01-14 14:39:35 -0500

    Update `assetsPublicPath` for bakanobingo.com

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/baka-no-bingo',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
