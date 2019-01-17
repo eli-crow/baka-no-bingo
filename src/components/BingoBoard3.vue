e4fbdfab36e295c74276a189585e74e351944bc5
Eli Crow <elimichaelcrow@gmail.com>
2019-01-17 16:39:42 -0500

    ⚙️ Transitions working for BingoBoard

--- src/components/BingoBoard3.vue
+++ src/components/BingoBoard3.vue
@@ -1,5 +1,9 @@
+
 <template>
-	<TransitionList class="BingoBoard3">
+	<transition-group class="BingoBoard3"
+	                  name="BingoBoard3"
+	                  tag="div"
+					  @before-leave="beforeLeave">
 		<div class="cell-container"
 		      v-for="(cell, i) in cells"
 		      :key="cell.key"
@@ -14,25 +18,49 @@
 				<span v-else class="text">{{ cell.text }}</span>
 			</div>
 		</div>
-	</TransitionList>
+	</transition-group>
 </template>
 
 
 
 <script>
-import TransitionList from '@/components/TransitionList'
-
 export default {
 	name: 'BingoBoard3',
-	components: {
-		TransitionList,
-	},
 	props: {
 		cells: Array,
 		enabled: {
 			type: Boolean,
 			default: true,
 		},
+	},
+	data () {
+		return {
+			staggerIndex: 0,
+		}
+	},
+	methods: {
+		beforeLeave (el) {
+			el.style.setProperty('--stagger-index', this.staggerIndex)
+			this.staggerIndex++
+		},
+		applyTransitionOffsetStyles() {
+			const children = Array.from(this.$el.children)
+			children.forEach(el => {
+				const {marginLeft, marginTop, width, height} = window.getComputedStyle(el)
+				const {offsetLeft, offsetTop} = el
+				el.style.left = `${offsetLeft - parseFloat(marginLeft, 10)}px`
+				el.style.top = `${offsetTop - parseFloat(marginTop, 10)}px`
+				el.style.width = width
+				el.style.height = height
+			})
+			this.staggerIndex = 0
+		}
+	},
+	updated () {
+		this.applyTransitionOffsetStyles()
+	},
+	mounted () {
+		this.applyTransitionOffsetStyles()
 	},
 }
 </script>
@@ -93,4 +121,55 @@ export default {
 .icon {
 	font-size: 20vw;
 }
+
+.BingoBoard3-enter-active,
+.BingoBoard3-enter-to,
+.BingoBoard3-leave-active {
+	--stagger-delay: calc(var(--stagger-index, 0) * 30ms);
+	--duration-transform: .35s;
+	--duration-delay: 0.2s;
+	--duration-opacity: 0.25s;
+	--easing-transform: cubic-bezier(0.740, 0.010, 0.455, 1.290);
+	transform-origin: center;
+}
+.BingoBoard3-enter-active,
+.BingoBoard3-enter-to {
+	transition:
+		opacity var(--duration-opacity) ease var(--stagger-delay),
+		transform var(--duration-transform) var(--easing-transform) calc(var(--duration-opacity) + var(--duration-delay) + var(--stagger-delay));
+}
+.BingoBoard3-leave-active {
+	transition:
+		opacity var(--duration-opacity) ease calc(var(--duration-transform) + var(--duration-delay) + var(--stagger-delay)),
+		transform var(--duration-transform) var(--easing-transform) var(--stagger-delay);
+}
+
+.BingoBoard3-leave,
+.BingoBoard3-leave-active { 
+	position: absolute !important;
+	z-index: 100 !important;
+}
+.BingoBoard3 > *:not(.BingoBoard3-leave-active) {
+	/* override these when not leaving, so we can set screen position for all cell-containers in `updated` lifecycle method */
+	top: auto !important;
+	left: auto !important;
+	width: auto !important;
+	height: auto !important;
+}
+
+.BingoBoard3-enter {
+	opacity: 0;
+	transform: translateY(1rem);
+}
+.BingoBoard3-leave-to {
+	opacity: 0;
+	transform: translateY(-8rem);
+}
+
+.BingoBoard3-move:not(.BingoBoard3-leave-active):not(.BingoBoard3-enter-active) {
+	transition: transform 0.35s ease;
+	z-index: 1;
+}
+</style>
+
 </style>
