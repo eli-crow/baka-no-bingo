dada9ecc2c60c5e1e0d40c669201e30b126d549d
Eli Crow <elimichaelcrow@gmail.com>
2019-01-22 09:07:35 -0500

    ⚡️ Use socket.io rooms

--- server/index.js
+++ server/index.js
@@ -2,7 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var uuid = require('uuid')
-var rooms = require('./rooms')
+
+const POSSIBLE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+function generateRoomId () {
+    let id
+    do {
+        id = ''
+        for (let i = 0; i < 4; i++) {
+            id += POSSIBLE_LETTERS.charAt(Math.floor(Math.random() * POSSIBLE_LETTERS.length))
+        }
+    } while (id in io.sockets.adapter.rooms)
+    return id
+}
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
@@ -10,20 +21,22 @@ app.get('/', function(req, res){
 
 io.on('connection', function (socket) {
     io.emit('CONNECT')
-    const playerId = uuid()
 
     socket.on('disconnect', function () {
         io.emit('DISCONNECT')
     })
     socket.on('host', function () {
-        const id = rooms.create()
+        const id = generateRoomId()
+        socket.join(id)
         socket.emit('CONFIRM_HOST', id)
     })
     socket.on('join', function (id) {
-        const success = rooms.join(id)
-        if (success) {
+        if (id in io.sockets.adapter.rooms) {
             socket.emit('CONFIRM_GUEST', id)
         }
+    })
+    socket.on('set_player_data', function (playerData) {
+        socket.playerData = playerData
     })
 })
 
